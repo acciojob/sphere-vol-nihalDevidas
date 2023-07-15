d563feee4044634b01e6b8b53c876436907e510b
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-16 01:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+    const radiusElement = document.getElementById("radius");
+    const volumnElement = document.getElementById("volume");
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+	const intVal = 0 - (-radiusElement.value);  
+  
+	
+	const vol = 4/3 *Math.PI * intVal**3;
+	
+  
+	volumnElement.value = vol+"";
+	
+} 
+document.getElementById('MyForm').onsubmit = volume_sphere;
